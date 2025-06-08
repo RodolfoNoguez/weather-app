5ed2df5378a4d3909c31aca90e6e2d32647aaf98
Rodolfo Noguez <rodolfonoguez10@gmail.com>
2025-06-07 22:18:26 -0500

    created the function to get weather lcoation details

--- src/main.js
+++ src/main.js
@@ -1,1 +1,22 @@
 import './style.css'
+
+async function getweatherlocation(location){
+    const apiKey = H3JNKYSNLTQTJBAGA9ZKARAK2;
+    const locationToFind = location;
+    const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${locationToFind}?key=${apiKey}&unitGroup=metric`;
+    try{
+        const response = await fetch(url);
+        const data = await response.json();
+
+        if(!response.ok){
+            throw new Error(data.error || data.details || `Weather data not found for "${location}"`)
+        }
+
+        return data;
+    }catch(err){
+        console.errir('Error fetching weather data:', err);
+    }
+}
+
+
+
